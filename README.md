9a2cbc1e1f6c8d4971b1c79df689e09d46590d6c
dcmonk3y <dcmonk3y@gmail.com>
2013-06-17 14:05:10 +0200

    Adds setup.py and updates README.

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 WatchFolder
 ===========
 
-Video, File sequence
+
+
+Features
+--------
+
+* folders monitoring
+* file sequence and video treatment
+
+
+
+Installation
+------------
+
+[setuptools](https://pypi.python.org/pypi/setuptools/0.7.2) is required.
+
+     python setup.py install
---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+
+setup(
+    name = 'WatchFolder',
+    version = '0.1.0',
+    description = ('Folder monitoring, file sequence and video treatment.'),
+    keywords = 'video file sequence',
+    packages=['watchfolder'],
+    long_description=read('README.md'),
+    install_requires=['watchdog']
+)
