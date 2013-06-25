fcd89d6b06fd68a35c2df20db91657b644b48cfd
to0ms <th.salvatore@gmail.com>
2013-06-25 16:56:37 +0200

    Adding log file and standard output for the info messages into Samples.

--- samples/allcallbacks.py
+++ samples/allcallbacks.py
@@ -1,9 +1,9 @@
 import os
 
-def dpxProcessor( file_path ):
-	print( "[EVENT]   removing dpx file : " + file_path )
-	os.remove( file_path )
+def dpxProcessor( filePath ):
+	logging.info("REMOVING .DPX FILE: %s", filePath)
+	os.remove( filePath )
 
-def movProcessor( file_path ):
-	print( "[EVENT]   removing mov file : "+ file_path )
-	os.remove( file_path )
+def movProcessor( filePath ):
+	logging.info("REMOVING .MOV FILE: %s", filePath)
+	os.remove( filePath )
--- samples/sample.py
+++ samples/sample.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
+
 import sys
 import time
 import os.path
 import argparse
 import logging
+import logging.handlers
 from monitor import Monitor
 
 parser = argparse.ArgumentParser( description='Watch Folder')
+
 parser.add_argument(
 	'-d', '--directory', default=".",
 	help='watched directory')
+
 parser.add_argument(
 	'-c', '--configuration', default="configuration.conf",
 	help='set the configuration file')
+
 parser.add_argument(
-		'-l', '--log', default="WARNING",
-		help='set the verbose level (critical, error, warning, info, debug)')
+	'-l', '--log', default="WARNING",
+	help='set the verbose level (critical, error, warning, info, debug)')
 
 args = parser.parse_args()
-
 numeric_level = getattr( logging, args.log.upper(), None)
 if not isinstance( numeric_level , int):
 	print ( 'Invalid log level: %s' % args.log.upper() )
@@ -30,15 +34,26 @@ if not isinstance( numeric_level , int):
 	print ( ' - debug' )
 	exit()
 
-logging.basicConfig(
-	#filename='example.log',
-	format='%(asctime)s - %(levelname)s - %(message)s',
-	datefmt='%m/%d/%Y %H:%M:%S',
-	level = numeric_level
-	)
+# ------
+
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', '%m/%d/%Y %H:%M:%S')
+
+handler = logging.handlers.RotatingFileHandler('watchFolder.log', 'a', 1000000, 1)
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(formatter)
+root_logger.addHandler(handler)
+
+stdout = logging.StreamHandler() 
+stdout.setLevel(numeric_level)
+stdout.setFormatter(formatter)
+root_logger.addHandler(stdout)
+
+# ------
 
 if not os.path.isdir( args.directory ):
-
 	logging.critical( " Could not start watching not on existing directory" )
 	exit( -1 )
 
